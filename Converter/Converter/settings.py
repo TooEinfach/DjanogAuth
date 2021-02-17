fe0c76ec44b8dc7518dd82973460cf3d3537223f
Nick <whiteshadow148@gmail.com>
2021-02-17 16:36:12 -0700

    Auth working

--- Converter/Converter/settings.py
+++ Converter/Converter/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'jsonConverter.apps.JsonconverterConfig',
     'rest_framework',
+    'rest_framework.authtoken', 
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',  
+    ],
+}
