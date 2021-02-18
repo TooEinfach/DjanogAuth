eec72eba71bff8e350f5451a9b32036a06a8e6c7
Nick Stenquist <whiteshadow148@gmail.com>
2021-02-17 21:24:39 -0700

    Post now authenticates and can request new token

--- Converter/jsonConverter/views.py
+++ Converter/jsonConverter/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from .models import Files
+import os
+
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
@@ -12,3 +16,10 @@ class DemoView(APIView):
     def get(self, request):
         content = {'message': 'Hello! This is a Demo!'}
         return Response(content)
+
+    def post(self, request):
+        if request.method == 'POST':
+            newdoc = Files(json_file=request.FILES.get('myfile'))
+            newdoc.save()
+            success = {'message': 'Post was successful'}
+            return Response(success)
